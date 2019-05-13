5123af4a51b3c33db80fb9384da3983163474920
mihael <rasperger01@gmail.com>
2019-05-13 18:24:48 +0200

    reset button, h1 color

--- ColorGameJs.js
+++ ColorGameJs.js
@@ -3,7 +3,22 @@ var squares = document.querySelectorAll(".square");       //select squares
 var pickedColor = pickColor();                              //pick 1 color,
 var colorDisplay=document.getElementById("colorDisplay");  //select in h1 Rgb
 var messageDisplay=document.querySelector("#message");     //under h1 try again/correct
-                         //colors from random colors func, it willl generate 6 colors
+var h1=document.querySelector("h1"); 
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function(){
+    //generate all new colors
+    colors=generateRandomColors(6);
+    //pick a new random color from array
+    pickedColor=pickColor();
+    //change colorDisplay to match picked color
+    colorDisplay.textContent= pickedColor;
+    //change color of squares
+    for (var i=0; i<squares.length; i++){
+        squares[i].style.backgroundColor=colors[i];
+    }
+    h1.style.backgroundColor= "#232323";
+})
 
 colorDisplay.textContent=pickedColor;                          //in h1 text content = picked color
 
@@ -17,9 +32,12 @@ for (var i=0; i <squares.length; i++) {                     //color squares
             //grab color of clicked square
            var clickedColor = this.style.backgroundColor;               //pick square and see whicj color it is
             //compare color to pickedColor
+            console.log(clickedColor,pickedColor);
             if (clickedColor===pickedColor) {
                messageDisplay.textContent="Correct!";             //if u pick right color u get under h1 correct
+               resetButton.textContent="Play Again?";              //if ya pick correct reset button will be "play again"                     
                changeColors(clickedColor);  //if u guess correctly all squares will be same colors
+               h1.style.backgroundColor=clickedColor;
             }
             else {
                 this.style.backgroundColor="#232323";  //if you click on the wrong one square.color=black
@@ -38,18 +56,11 @@ function changeColors(color)  {
     for (var i=0; i<squares.length; i++){
     //change all of them in same correct color
         squares[i].style.backgroundColor = color;
-
 }
-
 }
-
-
-
-
 
 //4.give to every square different random color
 function pickColor() {
-
 var random = Math.floor(Math.random() * colors.length);     //picks random number od o do array lenght,3 ili 6
 return colors[random];      //return color on that place in array colors[3]=yellow
 }
@@ -64,7 +75,7 @@ function randomColor() {
     //get blue 0-255
     var b = Math.floor(Math.random()*256);
     //putit together
-    return "rgb(" + r + "," + g + "," + b + ")";
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
 //2.RANDOM COLOR PUSH INTO ARRAY
@@ -75,7 +86,6 @@ function generateRandomColors(num){
     for (var i= 0; i<num ; i++) {
         //get random color and push into arr
         arr.push(randomColor())
-
 }
 //return that array
 return arr;
