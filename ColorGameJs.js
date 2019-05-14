3dab54dbda421faf50ff3e8d7db6a6ebc89ed93e
mihael <rasperger01@gmail.com>
2019-05-14 15:02:50 +0200

    easyBtn, hardBtn

--- ColorGameJs.js
+++ ColorGameJs.js
@@ -1,14 +1,51 @@
-var colors=generateRandomColors(6);
+var numSquares=6;
+var colors=generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");       //select squares
 var pickedColor = pickColor();                              //pick 1 color,
 var colorDisplay=document.getElementById("colorDisplay");  //select in h1 Rgb
 var messageDisplay=document.querySelector("#message");     //under h1 try again/correct
 var h1=document.querySelector("h1"); 
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function(){
+    easyBtn.classList.add("selected");              //when u clik on easBtn, add class .selected to easyBtn,remove from hardBtn
+    hardBtn.classList.remove("selected");
+    numSquares=3;
+    colors=generateRandomColors(numSquares);                //when u click generate 3 new colors
+    pickedColor=pickColor();
+    colorDisplay.textContent=pickedColor;
+    for (var i=0 ; i<squares.length; i++) {
+        if (colors[i]){                                         //loop goes only in first 3 squared, and we color them
+            squares[i].style.backgroundColor=colors[i];         //change only first 3 squares colors
+        }
+        else {squares[i].style.display="none";                      //hide last 3 squares
+        }
+    }
+});
+
+hardBtn.addEventListener("click", function(){
+    easyBtn.classList.remove("selected");
+    hardBtn.classList.add("selected");
+    numSquares=6;
+    colors=generateRandomColors(numSquares);                //when u click generate 6 new colors
+    pickedColor=pickColor();
+    colorDisplay.textContent=pickedColor;
+    for (var i=0 ; i<squares.length; i++) {
+                                             
+         squares[i].style.backgroundColor=colors[i];         //change all squares colors
+        
+         squares[i].style.display="block";                      //show last 3 squares
+        }
+    });
+
+
+
 
 resetButton.addEventListener("click", function(){
     //generate all new colors
-    colors=generateRandomColors(6);
+    colors=generateRandomColors(numSquares);
     //pick a new random color from array
     pickedColor=pickColor();
     //change colorDisplay to match picked color
